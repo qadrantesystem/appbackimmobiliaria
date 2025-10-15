ba615e440774bd4121b2c03f0edfd88c9b49bcbf
qadrantesystem <sistemas@qadrante2.com>
2025-10-14 19:32:03 -0500

    Fix: Agregar import Boolean en modelo Suscripcion

--- app/models/suscripcion.py
+++ app/models/suscripcion.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DECIMAL, TIMESTAMP, ForeignKey, CheckConstraint
+from sqlalchemy import Column, Integer, String, DECIMAL, Boolean, TIMESTAMP, ForeignKey, CheckConstraint
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.database import Base
